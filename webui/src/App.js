3d95cbfdf7b8b2564fc8b614325109d024093b39
bisbist <bistbishal69@gmail.com>
2022-09-25 17:27:28 +0545

    fetch text from server and display in ui

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = React.useState(null);
+  
+  React.useEffect(() => {
+    fetch('http://localhost:8000/')
+      .then(results => results.text())
+      .then(text => {
+        setText(text);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-          Sudoswap Frontend
+          { text }
       </header>
     </div>
   );
