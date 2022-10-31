4ae8079c53171919f0049d1086f780eec77db46e
bbist <pro.bbist@gmail.com>
2022-10-31 23:51:31 +0545

    save more details including bondingCurve, nft, poolType in pair db + optimize fetchMissingPairs

--- server/pairdb.js
+++ server/pairdb.js
@@ -38,98 +38,112 @@ const factoryContract = new ethers.Contract(info.factory.address, info.factory.a
 
 // create a mock json array db that represents a table with records
 const createPairDB = async (name) => {
-    try {
-        await fs.stat(name)
-    } catch (error) {
-        if (error.code == 'ENOENT') {
-            await fs.writeFile(name, JSON.stringify([]))
-        }
+  try {
+    await fs.stat(name)
+  } catch (error) {
+    if (error.code == 'ENOENT') {
+      await fs.writeFile(name, JSON.stringify([]))
     }
-    return {
-        async get(pair) {
-            const buf = await fs.readFile(name)
-            const json =  JSON.parse(buf.toString())
-            return json.find(obj => {
-                return obj.pair = pair
-            })
-        },
-        async getAll() {
-            const buf = await fs.readFile(name)
-            const json =  JSON.parse(buf.toString())
-            return json;
-        },
-        async put(pair, spotPrice, delta, fee, assetRecipient) {
-            const buf = await fs.readFile(name)
-            const json =  JSON.parse(buf.toString())
-            const index = json.findIndex(obj => obj.pair == pair)
-            if (index >= 0) { // found
-                json[index] = {pair, spotPrice, delta, fee, assetRecipient}
-            } else {
-                json.push({pair, spotPrice, delta, fee, assetRecipient})
-            }
-            await fs.writeFile(name, JSON.stringify(json, null, 2))
-        },
-    };
+  }
+  return {
+    async get(pair) {
+      const buf = await fs.readFile(name)
+      const json = JSON.parse(buf.toString())
+      return json.find(obj => {
+        return obj.pair = pair
+      })
+    },
+    async getAll() {
+      const buf = await fs.readFile(name)
+      const json = JSON.parse(buf.toString())
+      return json;
+    },
+    async put(pair, blockNumber, txHash, txIndex, logIndex, spotPrice, delta, fee, assetRecipient, bondingCurve, nft, poolType) {
+      const buf = await fs.readFile(name)
+      const json = JSON.parse(buf.toString())
+      const index = json.findIndex(obj => obj.pair == pair)
+      if (index >= 0) { // found
+        json[index] = { pair, blockNumber, txHash, txIndex, logIndex, spotPrice, delta, fee, assetRecipient, bondingCurve, nft, poolType }
+      } else {
+        json.push({ pair, blockNumber, txHash, txIndex, logIndex, spotPrice, delta, fee, assetRecipient, bondingCurve, nft, poolType })
+      }
+      await fs.writeFile(name, JSON.stringify(json, null, 2))
+    },
+  };
 }
 
-const fetchPairDetailsAndSave = async (db, pairAddress) => {
-  // insert pool information in db
-  const contract = new ethers.Contract(pairAddress, info.pair.abi, provider);
+// insert pool information in db
+const fetchPairDetailsAndSave = async (db, event) => {
+  const pair = event.args.poolAddress;
+
+  const contract = new ethers.Contract(pair, info.pair.abi, provider);
 
   // fetch default parameters
   const [
     spotPrice,
     delta,
     fee,
-    assetRecipient
+    assetRecipient,
+    bondingCurve,
+    nft,
+    poolType
   ] = await Promise.all([
     contract.spotPrice(),
     contract.delta(),
     contract.fee(),
     contract.assetRecipient(),
+    contract.bondingCurve(),
+    contract.nft(),
+    contract.poolType(),
   ])
 
   // fetch additional information: 
   // bondingCurve, nftAddress, poolType
 
   // save in db with addr as primary key
-//   console.log(pairAddress, 
-//     "spot:", spotPrice.toString(), 
-//     "delta:", delta.toString(), 
-//     'fee:', fee.toString(), 
-//     "recepient:", assetRecipient.toString())
-    db.put(pairAddress, 
-        spotPrice.toString(), 
-        delta.toString(), 
-        fee.toString(), 
-        assetRecipient.toString())
+  db.put(pair,
+    event.blockNumber,
+    event.transactionHash,
+    event.transactionIndex,
+    event.logIndex,
+    spotPrice.toString(),
+    delta.toString(),
+    fee.toString(),
+    assetRecipient.toString(),
+    bondingCurve.toString(),
+    nft.toString(),
+    poolType.toString()
+  )
 }
 
 
 
 // fetch existing NewPair events from blockchain
-const fetchMissingPairs = async (db ) => {
-    // fetch latest pair block number from db
-    const lastestPairBlockNumber = 1;
+const fetchMissingPairs = async (db) => {
+  // fetch latest pair block number from db
+  const pairs = await db.getAll()
+  const lastestPairBlockNumber = pairs.reduce((max, pair) => {
+    return pair.blockNumber > max ? pair.blockNumber : max
+  }, 1)
 
-    const existingPairs = await factoryContract.queryFilter(
-      factoryContract.filters.NewPair(),
-      lastestPairBlockNumber, // factoryContract.deployTransaction.blockNumber,
-      null
-    )
-
-    existingPairs.forEach(async (pair) => {
-      await fetchPairDetailsAndSave(db, pair.args.poolAddress)
-    })
+  const existingPairs = await factoryContract.queryFilter(
+    factoryContract.filters.NewPair(),
+    lastestPairBlockNumber, // factoryContract.deployTransaction.blockNumber,
+    null
+  )
+  
+  existingPairs.forEach(async (event) => {
+    await fetchPairDetailsAndSave(db, event)
+  })
 }
 
 // fetch new NewPair events from blockchain
 const fetchNewPairs = (db) => {
-    factoryContract.on(
-        factoryContract.filters.NewPair(), // on NewPair event
-        async (pair) => {
-            await fetchPairDetailsAndSave(db, pair)
-        })
+  factoryContract.on(
+    factoryContract.filters.NewPair(), // on NewPair event
+    async (pair, event) => {
+      await fetchPairDetailsAndSave(db, event)
+    })
 }
 
 export { createPairDB, fetchMissingPairs, fetchNewPairs };
