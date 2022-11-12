d44547a5e5908e27731580fd05f7ce0fdfd7c0b5
bisbist <bistbishal69@gmail.com>
2022-11-12 11:43:15 +0545

    fix styling and use SwapList for reading multiple swap inputs

--- webui/src/components/Swap/SwapETHForAnyNFTs.js
+++ webui/src/components/Swap/SwapETHForAnyNFTs.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { ethers } from 'ethers'
 import { provider, contracts } from '../../environment'
-import PairSwapAny from './components/PairSwapAny'
+import SwapList from './components/SwapList'
 
 
 const SwapETHForAnyNFTs = ({
     router: { name: routerName, createContract: createRouterContract },
 }) => {
 
-    const [swapList, setSwapList] = React.useState([{ pair: "", numItems: 0 }])     // PairSwapAny[] swapList
+    const [swapList, setSwapList] = React.useState([
+        { pair: "", numItems: 0 },
+    ])     // PairSwapAny[] swapList
     const [ethRecipient, setETHRecipient] = React.useState("")    // address payable ethRecipient
     const [nftRecipient, setNFTRecipient] = React.useState("")    // address nftRecipient
     const [deadline, setDeadline] = React.useState("0")    // uint256 deadline
@@ -16,90 +18,105 @@ const SwapETHForAnyNFTs = ({
 
     return (
         <div>
-            {
-                swapList.map((swap, i) => {
-                    return (
-                        <div key={i} style={{ display: "flex" }}>
-                            <PairSwapAny value={swap} onChange={value => {
-                                const newSwapList = [...swapList]
-                                newSwapList[i] = value
-                                setSwapList(newSwapList)
-                            }} />
-                            <button onClick={() => {
-                                const newSwapList = swapList.filter((_, index) => index != i)
-                                setSwapList(newSwapList)
-                            }}>x</button>
-                        </div>
-                    )
-                })
-            }
+            <table style={{
+                margin: 5, minWidth: 500,
+                border: 'solid', borderWidth: 1, borderColor: "red"
+            }}>
+                <thead>
+                    <tr>
+                        <th style={{ width: "30%" }} />
+                        <th style={{ width: "70%" }} />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>ETH Recipient</td>
+                        <td>
+                            <input
+                                style={{ width: "97.5%" }}
+                                value={ethRecipient}
+                                onChange={event => setETHRecipient(event.target.value)} />
+                        </td>
+                    </tr>
 
-            <button onClick={() => {
-                const newSwapList = [...swapList, { pair: "", numItems: 0 }]
-                setSwapList(newSwapList)
-            }}>Add Swap!</button>
+                    <tr>
+                        <td>NFT Recipient</td>
+                        <td>
+                            <input
+                                style={{ width: "97.5%" }}
+                                value={nftRecipient}
+                                onChange={event => setNFTRecipient(event.target.value)} />
+                        </td>
+                    </tr>
 
-            <div>
-                <input
-                    value={ethRecipient}
-                    onChange={event => setETHRecipient(event.target.value)} />
-            </div>
+                    <tr>
+                        <td>Deadline</td>
+                        <td>
+                            <input
+                                style={{ width: "97.5%" }}
+                                type="number" step={1} min={5}
+                                value={deadline}
+                                onChange={event => setDeadline(event.target.value)} />
+                        </td>
+                    </tr>
 
-            <div>
-                <input
-                    value={nftRecipient}
-                    onChange={event => setNFTRecipient(event.target.value)} />
-            </div>
+                    <tr>
+                        <td>Amount</td>
+                        <td>
+                            <input
+                                style={{ width: "97.5%" }}
+                                type="number" step={0.001} min={0}
+                                value={amount}
+                                onChange={event => {
+                                    setAmount(event.target.value)
+                                }} />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
-            <div>
-                <input
-                    type="number" step={1} min={5}
-                    value={deadline}
-                    onChange={event => setDeadline(event.target.value)} />
-            </div>
+            <SwapList
+                type="PairSwapAny"
+                swapList={swapList}
+                onChange={swapList => {
+                    console.log(swapList)
+                    setSwapList(swapList)
+                }} />
 
-            <div>
-                <input
-                    type="number" step={0.001} min={0}
-                    value={amount}
-                    onChange={event => {
-                        setAmount(event.target.value)
-                    }} />
-            </div>
+            <div style={{ textAlign: "center" }}>
+                <button onClick={async () => {
 
+                    await provider.send("eth_requestAccounts"); // connect specific metamask wallet with this site\
 
-            <button onClick={async () => {
+                    const signer = provider.getSigner()
 
-                await provider.send("eth_requestAccounts"); // connect specific metamask wallet with this site\
+                    const router = createRouterContract(signer)
 
-                const signer = provider.getSigner()
+                    let deadline = Date.now() + Math.floor(1000 * parseFloat(deadline))
 
-                const router = createRouterContract(signer)
-
-                let deadline = Date.now() + Math.floor(1000 * parseFloat(deadline))
-
-                if (ethRecipient == "") {
-                    ethRecipient = await signer.getAddress()
-                }
-
-                if (nftRecipient == "") {
-                    nftRecipient = await signer.getAddress()
-                }
-
-                let txn = await router.swapETHForSpecificNFTs(
-                    swapList.map(swap => [swap.pair, swap.numItems]),
-                    ethRecipient,
-                    nftRecipient,
-                    deadline,
-                    {
-                        value: ethers.utils.parseEther(amount),
-                        gasLimit: 30000000,
+                    if (ethRecipient == "") {
+                        ethRecipient = await signer.getAddress()
                     }
-                )
 
-                await txn.wait()
+                    if (nftRecipient == "") {
+                        nftRecipient = await signer.getAddress()
+                    }
 
-            }}>Send Transaction!</button>
+                    let txn = await router.swapETHForSpecificNFTs(
+                        swapList.map(swap => [swap.pair, swap.numItems]),
+                        ethRecipient,
+                        nftRecipient,
+                        deadline,
+                        {
+                            value: ethers.utils.parseEther(amount),
+                            gasLimit: 30000000,
+                        }
+                    )
+
+                    await txn.wait()
+
+                }}>Send Transaction!</button>
+            </div>
 
         </div>
     )
