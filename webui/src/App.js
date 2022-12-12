68ad040fb6b20ee0a5c60778c62ce7acad58fad4
bisbist <bistbishal69@gmail.com>
2022-12-12 17:30:16 +0545

    corrected code structure

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,35 +1,28 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import { setConfig } from './config';
-import Home from './pages/Home'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { setConfig } from "./config";
+import Home from "./pages/Home";
+import "@blueprintjs/core/lib/css/blueprint.css";
 
 function App() {
-
-  const [initialized, setInitialized] = React.useState(false)
-
+  const [initialized, setInitialized] = React.useState(false);
 
   React.useEffect(() => {
-
     fetch("/config")
-      .then(res => res.json())
-      .then(cfg => {
-        setConfig(cfg)
-        setInitialized(true)
-      })
-  }, [])
+      .then((res) => res.json())
+      .then((cfg) => {
+        setConfig(cfg);
+        setInitialized(true);
+      });
+  }, []);
 
   return initialized ? (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
-
     </BrowserRouter>
-  ) : null
+  ) : null;
 }
 
 export default App;
