4c3377f37a9c5131751d0a667c2dae1cf35c8419
PantaAastha <ap.pantaaastha@gmail.com>
2022-12-30 10:15:46 +0545

    fix: Minor bug fix

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { setConfig } from "./config";
-import Home from "./pages/Home";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import PoolCreator from "./components/PoolCreator";
 import NavBar from "./components/Navbar/Navbar";
 import Swap from "./components/Swap";
 import PoolsTable from "./components/NewPoolsTable";
+import { Navigate } from "react-router-dom";
 
 function App() {
   const [initialized, setInitialized] = React.useState(false);
@@ -24,8 +24,11 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pools" element={[<PoolCreator />, <PoolsTable/>]} />
+        <Route path="/" element={<Navigate to="/pools" replace />} />
+        <Route
+          path="/pools"
+          element={[<PoolCreator key={"one"} />, <PoolsTable key={"two"} />]}
+        />
         <Route path="/swap" element={<Swap />} />
       </Routes>
     </BrowserRouter>
